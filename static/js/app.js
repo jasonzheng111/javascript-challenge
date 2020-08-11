72f29a855c560f192feddbdd2ff2f11da910411b
jasonzheng111 <61761046+jasonzheng111@users.noreply.github.com>
2020-08-11 11:17:57 -0400

    js homework

---
+++ static/js/app.js
@@ -0,0 +1,37 @@
+// from data.js
+var tableData = data;
+
+
+const Tablerows = d3.select("tbody");
+
+function showRows(data){
+    Tablerows.html("");
+    
+    // Now need to append the rows each by each
+    data.forEach((dataRow) => {
+        const Temp = Tablerows.append("tr");
+
+        Object.values(dataRow).forEach((dataelement) => {
+            let cell = Temp.append("td");
+                cell.text(dataelement);
+        }
+    );
+  });
+}
+
+
+function clickbehav() {
+
+    const specificdate = d3.select("#datetime").property("value");
+    let selectdata = tableData;
+  
+
+    if (specificdate) {
+        selectdata = selectdata.filter(eachrow => eachrow.datetime === specificdate);
+    }
+    showRows(selectdata);
+  }
+  
+d3.select("#filter-btn").on("click", clickbehav);
+
+showRows(tableData);
